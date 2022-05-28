92c5737a9d04def3f95042c1ae0b11e70ab305b3
NizamovaRegina <nizamova-2001@bk.ru>
2022-05-29 00:14:42 +0500

    create sort

--- Agents.xaml.cs
+++ Agents.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Imaging;
+using System.Windows.Data;
 
 namespace Poprygunchic
 {
@@ -11,18 +13,71 @@ namespace Poprygunchic
         public Agents()
         {
             InitializeComponent();
+            List<string> sourceSortComboBox = new List<string>
+            {
+               "Не сортировать",
+               "Наименование (А-Я)",
+               "Наименование (Я-А)",
+               "Размер скидки (мин.-макс.)",
+               "Размер скидки (макс.-мин.)",
+               "Приоритет агента (мин.-макс.)",
+               "Приоритет агента (макс.-мин.)"
+            };
+
             agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.ToList();
-           
+            sortComboBox.ItemsSource = sourceSortComboBox;
+
         }
 
-        //private void Pages()
-        //{
-        //    var agents = PoprygunchicEntities.GetContext().Agents.ToList();
+        private void SortComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e) //Сортировка
+        {
+            string comboBoxContent = sortComboBox.SelectedItem.ToString();
 
-        //    List<Agent> tenAgents = new List<Agent>();
+            switch (comboBoxContent)
+            {
+                case "Не сортировать":
+                {
+                    agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.ToList();
+                    break;
+                }
 
-        //    if (agents.Count > 10)
-        //        tenAgents.Add(agents.Skip(10).ToList());
-        //}
+                case "Наименование (А-Я)":
+                {
+                    agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.OrderBy(x => x.Title).ToList();
+                    break;
+                }
+
+                case "Наименование (Я-А)":
+                {
+                    agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.OrderByDescending(x => x.Title).ToList();
+                    break;
+                }
+
+                case "Размер скидки (мин.-макс.)":
+                {
+                    agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.OrderBy(x => x.PersentCount).ToList();
+                    break;
+                }
+
+                case "Размер скидки (макс.-мин.)":
+                {
+                    agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.OrderByDescending(x => x.PersentCount).ToList();
+                    break;
+                }
+
+                case "Приоритет агента (мин.-макс.)":
+                {
+                    agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.OrderBy(x => x.Priority).ToList();
+                    break;
+                }
+
+                case "Приоритет агента (макс.-мин.)":
+                {
+                    agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.OrderByDescending(x => x.Priority).ToList();
+                    break;
+                }
+            }
+
+        }
     }
 }
