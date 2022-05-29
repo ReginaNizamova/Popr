7ecd0826fe3b95846746d2193cae459dfc7e9410
NizamovaRegina <nizamova-2001@bk.ru>
2022-05-29 22:30:30 +0500

    create filter and search

--- Agents.xaml.cs
+++ Agents.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,10 +21,21 @@ namespace Poprygunchic
                "Приоритет агента (мин.-макс.)",
                "Приоритет агента (макс.-мин.)"
             };
+            List<string> sourceFilterComboBox = new List<string>
+            {
+               "Все типы",
+               "МКК",
+               "ОАО",
+               "ООО",
+               "ЗАО",
+               "МФО",
+               "ПАО"
+
+            };
 
             agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.ToList();
             sortComboBox.ItemsSource = sourceSortComboBox;
-
+            filterComboBox.ItemsSource = sourceFilterComboBox;
         }
 
         private void SortComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e) //Сортировка
@@ -55,13 +64,13 @@ namespace Poprygunchic
 
                 case "Размер скидки (мин.-макс.)":
                 {
-                    agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.OrderBy(x => x.PersentCount).ToList();
+                    agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.ToList().OrderBy(x => x.PersentCount).ToList();
                     break;
                 }
 
                 case "Размер скидки (макс.-мин.)":
                 {
-                    agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.OrderByDescending(x => x.PersentCount).ToList();
+                    agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.ToList().OrderByDescending(x => x.PersentCount).ToList();
                     break;
                 }
 
@@ -79,5 +88,32 @@ namespace Poprygunchic
             }
 
         }
+
+        private void FilterComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e) //Фильтрация
+        {
+            string currentItem = filterComboBox.SelectedItem.ToString();
+
+            if (currentItem == "Все типы")
+                agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.ToList();
+            else
+                agentsListView.ItemsSource = PoprygunchicEntities.GetContext().Agents.Where(x => x.AgentType.Title.Contains(currentItem)).ToList();
+        }
+
+        private bool AgentSearch (object item) //Поиск
+        {
+            if (string.IsNullOrEmpty(searchTextBox.Text))
+                return true;
+            else
+                return (item as Agent).Title.IndexOf(searchTextBox.Text, System.StringComparison.OrdinalIgnoreCase) > 0 ||
+                       (item as Agent).Phone.IndexOf(searchTextBox.Text, System.StringComparison.OrdinalIgnoreCase) > 0 ||
+                       (item as Agent).Email.IndexOf(searchTextBox.Text, System.StringComparison.OrdinalIgnoreCase) > 0;
+        }
+
+        private void SearchTextBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(agentsListView.ItemsSource);
+            view.Filter = AgentSearch;
+            CollectionViewSource.GetDefaultView(agentsListView.ItemsSource).Refresh();
+        }   
     }
 }
